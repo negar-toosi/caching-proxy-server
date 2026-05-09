d04dbaf360f4d626b419d7bff7a3ba5f9f5f7dcf
Negar Toosi <negar.tusi@gmail.com>
2026-05-09 16:59:15 +0330

    feat(setup): set logs on project #5

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+import logging
+
 import asyncio
 import socket
 import os
@@ -8,6 +10,8 @@ from dotenv import load_dotenv
 import signal
 
 load_dotenv()
+
+logger = logging.getLogger("server")
 
 
 class GracefulExit(SystemExit):
@@ -34,33 +38,41 @@ class WebsocketServer:
             server_socket.bind((self._host, self._port))
 
             server_socket.listen()
+            logger.info(f"server start with host {self._host} and port {self._port} ")
 
             for singname in {"SIGINT", "SIGTERM"}:
                 self._loop.add_signal_handler(getattr(signal, singname), shutdown)
             await self._connection_listener(server_socket)
         except Exception as ex:
+            logger.exception(f"an unexpected error happen: {ex}")
             raise ex
 
     async def _connection_listener(self, server_socket: socket):
         while True:
             connection, address = await self._loop.sock_accept(server_socket)
             connection.setblocking(False)
-            print(f"Got a connection from address {address}")
+            logger.info(f"Got a connection from address {address}")
             task = asyncio.create_task(self._task(connection))
+            logger.info(f"A new task created for connection {address}")
             self._tasks.append(task)
 
     async def _task(self, connection: socket):
         try:
             data = await self._loop.sock_recv(connection, 1024)
+            logger.info(f"Get {data} data from connection {connection}")
             response = await request_task(data)
             await self._loop.sock_sendall(connection, bytes(response, "utf-8"))
+            logger.info(f"send response for connection {connection.AddressInfo}")
         finally:
             connection.close()
+            logger.info(f"Close the connection {connection}")
 
     async def close_tasks(self):
         waiters = [asyncio.wait_for(task, 2) for task in self._tasks]
+        logger.info("Start to close all task after user close the connection")
         for task in waiters:
             try:
                 await task
             except asyncio.exceptions.TimeoutError:
                 pass
+        logger.info("Finish close all tasks")
